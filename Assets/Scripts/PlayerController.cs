4df2fbdb070271822a82f17141cac9c904cf2ee9
ddgond <dadangond@gmail.com>
2015-05-23 14:45:00 -0400

    Removed player controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,22 +2,14 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
-	public float speed = 5f;
-	
-	private Rigidbody rb;
 	
 	void Start ()
 	{
-		rb = GetComponent<Rigidbody>();
+
 	}
 	
 	void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
 
-		Vector3 movement = new Vector3 (moveHorizontal * speed, rb.velocity.y, 0.0f);
-		
-		rb.velocity = movement;
 	}
 }
