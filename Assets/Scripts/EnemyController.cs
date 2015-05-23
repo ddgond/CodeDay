342694dfa52924b57ac8aaea1d80ff9bc199a4d1
ddgond <dadangond@gmail.com>
2015-05-23 16:17:11 -0400

    Shot follows

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,20 +3,22 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 
-	public float fireRate = 3f;
+	public float fireRate = 2f;
 	public GameObject shot;
 
 	private float timeSinceFire = 0f;
+	private GameObject player;
 
 	void Start () {
-
+		player = GameObject.Find ("Player");
 	}
 
 	void Update () {
 		timeSinceFire += Time.deltaTime;
 		if (timeSinceFire > fireRate) {
 			timeSinceFire = 0f;
-			Instantiate(shot, transform.position, transform.rotation);
+			GameObject thisShot = Instantiate(shot, transform.position, transform.rotation) as GameObject;
+			thisShot.GetComponent<Mover>().Target(player);
 		}
 	}
 }
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,9 +5,20 @@ public class Mover : MonoBehaviour {
 
 	public float speed = 5;
 
+	private Rigidbody rb;
+
+	void Awake () {
+		rb = GetComponent<Rigidbody> ();
+		rb.velocity = transform.up * speed;
+	}
+
 	void Start () {
-		GetComponent<Rigidbody>().velocity = transform.up * speed;
-	
+
+	}
+
+	public void Target (GameObject target) {
+		transform.rotation = Quaternion.LookRotation (target.transform.position - transform.position) * Quaternion.Euler (0, 90, 90);
+		rb.velocity = (target.transform.position - transform.position).normalized * speed;
 	}
 
 }
