f69ab287b20ab76675fcc20ba2ea432e96379fff
Sam Tang <17tangs@my.weston.org>
2015-05-23 14:51:59 -0400

    Fires

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,23 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 5f;
-	
 	private Rigidbody rb;
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
 	
+	private float nextFire;
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
+	}
+	void Update ()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
 	}
 	
 	void FixedUpdate ()
