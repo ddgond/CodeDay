0585894e255ca6780734aeaa789cfc79edd954b2
ddgond <dadangond@gmail.com>
2015-05-23 16:54:05 -0400

    Stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,7 +19,7 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetButton("Fire1") && Time.time > nextFire)
 		{
 			nextFire = Time.time + fireRate;
-			/*GameObject */thisShot = Instantiate(shot, shotSpawn.position, shotSpawn.rotation) /*as GameObject*/;
+			/*GameObject thisShot = */Instantiate(shot, shotSpawn.position, shotSpawn.rotation) /*as GameObject*/;
 			/*thisShot.GetComponent<Mover>().Target(Input.mouse);
 			thisShot.GetComponent<Mover>().spawnedBy = gameObject.name;*/
 		}
