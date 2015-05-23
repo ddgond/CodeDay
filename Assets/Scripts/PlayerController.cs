19732f086b8f8522889eca9f9d6a887c81b6b003
ddgond <dadangond@gmail.com>
2015-05-23 14:56:52 -0400

    Stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,25 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
 	
+	private float nextFire;
+
 	void Start ()
 	{
 
+	}
+	void Update ()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
 	}
 	
 	void FixedUpdate ()
