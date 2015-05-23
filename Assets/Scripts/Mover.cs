f057eb9a27360988583fc4a6d7e3ce1fdac2801a
Sam Tang <17tangs@my.weston.org>
2015-05-23 14:18:42 -0400

    Bullets added

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	public float speed = 5;
+
+	void Start () {
+		GetComponent<Rigidbody>().velocity = transform.up * speed;
+	
+	}
+
+}
