e5aa0729d901232880dcf368297acbd9b8fc1a8f
ddgond <dadangond@gmail.com>
2015-05-23 14:52:19 -0400

    Camera follows now

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public float smoothing = 0.2f;
 	
+	private GameObject player;
+	private Rigidbody rb;
+	private Vector3 velocity;
+	private float z;
+	
+	void Awake () {
+		player = GameObject.Find ("Player");
+		rb = gameObject.GetComponent<Rigidbody> ();
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Start () {
+		velocity = Vector3.zero;
+		z = transform.position.z;
+	}
 	
+	void LateUpdate () {
+		velocity = rb.velocity;
+		Follow ();
+	}
+	
+	void Follow () {
+		transform.position = Vector3.SmoothDamp (transform.position, player.transform.position, ref velocity, smoothing);
+		transform.position = new Vector3 (transform.position.x, transform.position.y, z);
 	}
 }
