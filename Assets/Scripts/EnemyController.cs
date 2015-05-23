a8c6810ee799bf20c34252bed940cc8b7f1c08f8
ddgond <dadangond@gmail.com>
2015-05-23 15:45:54 -0400

    Enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	public float fireRate = 3f;
+	public GameObject shot;
+
+	private float timeSinceFire = 0f;
+
+	void Start () {
+
+	}
+
+	void Update () {
+		timeSinceFire += Time.deltaTime;
+		if (timeSinceFire > fireRate) {
+			timeSinceFire = 0f;
+			Instantiate(shot, transform.position, transform.rotation);
+		}
+	}
+}
