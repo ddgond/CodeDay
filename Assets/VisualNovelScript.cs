3606b0c0f749ae76767d70c07988c98241933b2e
ddgond <dadangond@gmail.com>
2015-05-24 00:39:31 -0400

    fhirfr

--- Assets/VisualNovelScript.cs
+++ Assets/VisualNovelScript.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class VisualNovelScript : MonoBehaviour {
 
-	// Use this for initialization
+	public List<string> dialogLines;
+	public float delay = 1f;
+
+	private float timePassed;
+	private int index;
+	private Text dialog;
+
+	void Awake () {
+		dialog = GameObject.Find ("Dialog").GetComponent<Text>();
+		index = 0;
+	}
+
 	void Start () {
-	
+		timePassed = 0f;
+		dialog.text = dialogLines[index];
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
-	
+		timePassed += Time.deltaTime;
+		if (timePassed > delay) {
+			if (Input.GetKey (KeyCode.Space)) {
+				timePassed = 0f;
+				index++;
+				if (index < dialogLines.Count)
+					dialog.text = dialogLines [index];
+			}
+		}
 	}
 }
